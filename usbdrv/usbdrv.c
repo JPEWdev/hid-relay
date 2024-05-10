56c7e46fbc6c9a44080e7e8731bde68ef992a6e6
Joshua Watt <JPEWhacker@gmail.com>
2024-05-10 11:35:01 -0600

    Add CRC check
    Adds a check to validate the CRC of all received packets. This is too
    late to ignore the packet as V-USB has already sent the ACK, but it will
    prevent the relay from operating on bad data.

--- usbdrv/usbdrv.c
+++ usbdrv/usbdrv.c
@@ -428,6 +428,10 @@ usbRequest_t    *rq = (void *)data;
  * 0...0x0f for OUT on endpoint X
  */
     DBG2(0x10 + (usbRxToken & 0xf), data, len + 2); /* SETUP=1d, SETUP-DATA=11, OUTx=1x */
+#if CHECK_CRC
+    if (usbCrc16(data, len + 2) != 0x4FFE)
+        return;
+#endif
     USB_RX_USER_HOOK(data, len)
 #if USB_CFG_IMPLEMENT_FN_WRITEOUT
     if(usbRxToken < 0x10){  /* OUT to endpoint != 0: endpoint number in usbRxToken */
